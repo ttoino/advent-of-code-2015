981943ec512afa4385e40a398fb54deefcba102c
toino <joaoapereira21@hotmail.com>
2022-08-12 19:05:10 +0100

    Add solutions to the day 9 puzzles

---
+++ day09/part2.py
@@ -0,0 +1,27 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter
+
+with open("input") as inf, open("part2.out", "w+") as outf:
+    graph = {}
+
+    for i in inf:
+        start, _, end, _, dist = i.strip().split()
+
+        graph.setdefault(start, {})
+        graph.setdefault(end, {})
+
+        graph[start][end] = int(dist)
+        graph[end][start] = int(dist)
+
+    m = 0
+    for p in it.permutations(graph.keys(), 8):
+        s = 0
+        for start, end in it.pairwise(p):
+            s += graph[start][end]
+        m = max(s, m)
+
+    outf.write(str(m))
