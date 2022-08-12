ed198a954efe0f07d03e2119e079ad236076c70b
toino <joaoapereira21@hotmail.com>
2022-08-12 13:56:36 +0100

    Add solutions to the day 4 puzzles

---
+++ day04/part1.py
@@ -0,0 +1,16 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter
+from hashlib import md5
+
+with open("input") as inf, open("part1.out", "w+") as outf:
+    inp = inf.readline().strip()
+
+    for i in it.count():
+        if md5(bytes(f"{inp}{i}",
+                     encoding="ascii")).hexdigest().startswith("00000"):
+            outf.write(str(i))
+            break
