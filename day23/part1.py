672e187aeb07b87b6ef8f81cc6cc208b85ee122b
toino <joaoapereira21@hotmail.com>
2022-08-15 20:18:08 +0100

    Add solutions to the day 23 puzzles

---
+++ day23/part1.py
@@ -0,0 +1,35 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter
+
+with open("input") as inf, open("part1.out", "w+") as outf:
+    instructions = [re.split(r",? ", i.strip()) for i in inf]
+
+    registers = { "a": 0, "b": 0 }
+    ip = 0
+
+    while 0 <= ip < len(instructions):
+        match instructions[ip]:
+            case ["hlf", r]:
+                registers[r] //= 2
+            case ["tpl", r]:
+                registers[r] *= 3
+            case ["inc", r]:
+                registers[r] += 1
+            case ["jmp", offset]:
+                ip += int(offset)
+                continue
+            case ["jie", r, offset]:
+                if registers[r] % 2 == 0:
+                    ip += int(offset)
+                    continue
+            case ["jio", r, offset]:
+                if registers[r] == 1:
+                    ip += int(offset)
+                    continue
+        ip += 1
+
+    outf.write(str(registers["b"]))
